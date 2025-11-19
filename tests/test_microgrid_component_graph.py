b3b2247d2168f352eb2a455f7db167c55f9ce12a
Sahas Subramanian <sahas.subramanian@proton.me>
2025-11-19 17:43:53 +0100

    Update build-system to maturin and expose ComponentGraph to python
    Also add tests to make sure graph creation and formula creation work.
    
    Signed-off-by: Sahas Subramanian <sahas.subramanian@proton.me>

--- tests/test_microgrid_component_graph.py
+++ tests/test_microgrid_component_graph.py
@@ -3,9 +3,77 @@
 
 """Tests for the frequenz.microgrid_component_graph package."""
 
+from frequenz.client.common.microgrid import MicrogridId
+from frequenz.client.common.microgrid.components import ComponentId
+from frequenz.client.microgrid.component import (
+    Component,
+    ComponentConnection,
+    GridConnectionPoint,
+    Meter,
+    SolarInverter,
+)
+
 from frequenz import microgrid_component_graph
 
 
-def test_loading() -> None:
+def test_graph_creation() -> None:
     """Test that the microgrid_component_graph module loads correctly."""
-    assert microgrid_component_graph is not None
+    graph: microgrid_component_graph.ComponentGraph[
+        Component, ComponentConnection, ComponentId
+    ] = microgrid_component_graph.ComponentGraph(
+        components={
+            GridConnectionPoint(
+                id=ComponentId(1),
+                microgrid_id=MicrogridId(1),
+                rated_fuse_current=100,
+            ),
+            Meter(id=ComponentId(2), microgrid_id=MicrogridId(1)),
+            Meter(id=ComponentId(3), microgrid_id=MicrogridId(1)),
+            SolarInverter(id=ComponentId(4), microgrid_id=MicrogridId(1)),
+        },
+        connections={
+            ComponentConnection(source=ComponentId(1), destination=ComponentId(2)),
+            ComponentConnection(source=ComponentId(1), destination=ComponentId(3)),
+            ComponentConnection(source=ComponentId(2), destination=ComponentId(4)),
+        },
+    )
+    assert graph.components() == {
+        GridConnectionPoint(
+            id=ComponentId(1), microgrid_id=MicrogridId(1), rated_fuse_current=100
+        ),
+        Meter(id=ComponentId(2), microgrid_id=MicrogridId(1)),
+        Meter(id=ComponentId(3), microgrid_id=MicrogridId(1)),
+        SolarInverter(id=ComponentId(4), microgrid_id=MicrogridId(1)),
+    }
+    assert graph.connections() == {
+        ComponentConnection(source=ComponentId(1), destination=ComponentId(2)),
+        ComponentConnection(source=ComponentId(1), destination=ComponentId(3)),
+        ComponentConnection(source=ComponentId(2), destination=ComponentId(4)),
+    }
+    assert graph.components(matching_ids=[ComponentId(2), ComponentId(3)]) == {
+        Meter(id=ComponentId(2), microgrid_id=MicrogridId(1)),
+        Meter(id=ComponentId(3), microgrid_id=MicrogridId(1)),
+    }
+    assert graph.components(matching_ids=ComponentId(1)) == {
+        GridConnectionPoint(
+            id=ComponentId(1), microgrid_id=MicrogridId(1), rated_fuse_current=100
+        )
+    }
+    assert graph.components(matching_types=Meter) == {
+        Meter(id=ComponentId(2), microgrid_id=MicrogridId(1)),
+        Meter(id=ComponentId(3), microgrid_id=MicrogridId(1)),
+    }
+    assert graph.components(matching_types=[Meter, GridConnectionPoint]) == {
+        Meter(id=ComponentId(2), microgrid_id=MicrogridId(1)),
+        Meter(id=ComponentId(3), microgrid_id=MicrogridId(1)),
+        GridConnectionPoint(
+            id=ComponentId(1), microgrid_id=MicrogridId(1), rated_fuse_current=100
+        ),
+    }
+    assert graph.components(
+        matching_types=[Meter, SolarInverter],
+        matching_ids=[ComponentId(1), ComponentId(3), ComponentId(4)],
+    ) == {
+        Meter(id=ComponentId(3), microgrid_id=MicrogridId(1)),
+        SolarInverter(id=ComponentId(4), microgrid_id=MicrogridId(1)),
+    }
